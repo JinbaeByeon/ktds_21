1c2c0c79b8697efc2ee7afa2126ce62b006ba736
JinbaeByeon <quswlsqo@gmail.com>
2023-01-30 13:28:34 +0900

    CSV 실습, addressbook 추가

---
+++ lotto/src/lotto/CSV.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CSV {
+
+	public static void main(String[] args) {
+		try {
+			// 1. 파일을 읽어온다.
+			List<String> csv = Files.readAllLines(Paths.get("C:\\Users\\User\\Desktop\\점수표.csv"));
+
+			// 2. 라인별로 반복한다.
+			for (String line : csv) {
+				if (line.equals(csv.get(0))) {
+					continue;
+				}
+				// 라인을 ","로 분리한다.
+				String[] words = line.split(",");
+				// 분리된 텍스트를 반복하여 출력한다.
+//				for (String word : words) {
+//					System.out.printf(word + " ");
+//				}
+				System.out.println(words[0] + "과목의 점수는 "+ words[1] + "점이고, 등급은 " +words[2] +"입니다." );
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- lotto/src/lotto/Lotto.java
+++ lotto/src/lotto/Lotto.java
@@ -3,22 +3,57 @@ package lotto;
 public class Lotto {
 
 	public static void main(String[] args) {
-		int[] lottoArray = new int[6];
-		lottoArray[0] = 5;
-		lottoArray[1] = 15;
-		lottoArray[2] = 27;
-		lottoArray[3] = 34;
-		lottoArray[4] = 36;
-		lottoArray[5] = 41;
-		
+//		int[] lottoArray = new int[6];
+//		lottoArray[0] = 5;
+//		lottoArray[1] = 15;
+//		lottoArray[2] = 27;
+//		lottoArray[3] = 34;
+//		lottoArray[4] = 36;
+//		lottoArray[5] = 41;
+		int[] lottoArray = { 5, 15, 27, 34, 36, 41 };
+
 		// 전통적인 for
 		for (int i = 0; i < lottoArray.length; ++i) {
-			System.out.println(lottoArray[i]);						
+			System.out.println("lottoArray[" + i + "] = " + lottoArray[i]);
 		}
-		
+
 		// 성능이 빠른 foreach
-		for (int i : lottoArray) {
-			System.out.println(i);
+		for (int number : lottoArray) {
+			System.out.println(number);
 		}
+
+		// 반복범위가 명확한 데이터에 대해서는 이렇게 안함
+		int j = 0;
+		while (j < lottoArray.length) {
+			System.out.println(lottoArray[j++]);
+		}
+
+		String str = "8. 핵무기가 완전히 없어졌고, 군축까지  착실하게 이행한 우크라이나는 전력이 크게 약해졌지만, 러시아는 침공을 했고, 미국과 영국이 지켜준다는 약속은 이행되지 않았음. \r\n"
+				+ "[출처] 러시아 vs 우크라이나 전쟁 근황|작성자 메르";
+		String[] wordArray = str.split(" ");
+
+		for (String word : wordArray) {
+			System.out.println(word);
+		}
+		for (int i = 0; i < wordArray.length; ++i) {
+			if (wordArray[i].length() > 3) {
+				System.out.println(wordArray[i]);
+			}
+		}
+
+		// 홀수만 출력
+//		for (int i = 0; i < 100; ++i) {
+//			if (i % 2 == 1) {
+//				System.out.println("홀수: " + i);
+//			}
+//		}
+
+		// 홀수만 출력
+//		for (int i = 0; i <= 100; ++i) {
+//			if (i % 2 == 0) {
+//				System.out.println("짝수: " + i);
+//			}
+//		}
+
 	}
 }
